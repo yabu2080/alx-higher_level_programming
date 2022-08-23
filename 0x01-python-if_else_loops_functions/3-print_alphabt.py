5150abbc90e62e1569239cf1c76882dcecd120e8
yabu2080 <kidistkifleyohannes364@gmail.com>
2022-08-23 15:59:53 -0700

    bays

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end="")
+    if i == 101 or i == 113:
+        continue
+    print("{:c}".format(i), end='')
