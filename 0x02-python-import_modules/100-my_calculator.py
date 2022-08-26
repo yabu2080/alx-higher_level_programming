2ca1d1e4e623c7a3ba382bef958a0542be0acf66
yabu2080 <kidistkifleyohannes364@gmail.com>
2022-08-25 18:26:12 -0700

    last

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-def main(argv):
-    argc = len(argv)
-    ops = {
-        '+': calculator_1.add,
-        '-': calculator_1.sub,
-        '*': calculator_1.mul,
-        '/': calculator_1.div,
-    }
-    if argc != 4:
-        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
-        exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    op = argv[2]
-    if op not in '+-*/':
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-    res = ops[op](a, b)
-    print('{:d} {:s} {:d} = {:d}'.format(a, op, b, res))
-
-
 if __name__ == '__main__':
-    from sys import argv, exit
-    import calculator_1
-    main(argv)
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        ope = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
+
+        if ope == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif ope == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif ope == "*":
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        elif ope == '/':
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
