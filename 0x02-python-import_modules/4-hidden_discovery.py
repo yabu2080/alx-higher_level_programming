04c596273c2b180c157332e71382e31822db6df5
yabu2080 <kidistkifleyohannes364@gmail.com>
2022-08-25 17:43:57 -0700

    try

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name.startswith('__'):
+
+    for i in dir(hidden_4):
+        if i[:2] == "__":
             continue
-        print("{:s}".format(name))
+        print(i)
