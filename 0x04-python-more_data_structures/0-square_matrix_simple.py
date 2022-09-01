ecedd0fb4f618c719af4bdc9edbcf01f15b3b818
yabu2080 <kidistkifleyohannes364@gmail.com>
2022-08-31 18:34:06 -0700

    lays

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """A function that computes the square value. Use Map and Lambda"""
-    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
+    tmp = []
+    for x in matrix:
+        tmp.append(list(map(lambda x: x**2, x)))
+    return (tmp)
