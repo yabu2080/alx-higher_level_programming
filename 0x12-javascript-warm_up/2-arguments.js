3f108390ad8b5e0aee147af42bd8ff160b752e23
yabu2080 <kidistkifleyohannes364@gmail.com>
2022-11-28 14:52:13 -0800

    try

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const count = process.argv.length;
 console.log(
-	  count === 2
-	    ? 'No argument'
-	    : count === 3
-	      ? 'Argument found'
-	      : 'Arguments found'
+  count === 2
+    ? 'No argument'
+    : count === 3
+      ? 'Argument found'
+      : 'Arguments found'
 );
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 console.log(
-	  typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]
+  typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]
 );
