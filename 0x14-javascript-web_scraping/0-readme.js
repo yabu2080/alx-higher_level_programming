05bb2270a66d83ea7121fd9e3b510071aa82ffc7
yabu2080 <kidistkifleyohannes364@gmail.com>
2023-01-18 02:06:56 +0000

    try

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const URL = process.argv[2];
 
-request(URL, function (err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + response.statusCode);
-  }
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
