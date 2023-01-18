33f54f2282ca5d340365a3248d031a7a417c7573
yabu2080 <kidistkifleyohannes364@gmail.com>
2023-01-18 01:58:52 +0000

    again

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,9 +6,9 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, function (err, response, body) {
-	if (err) {
-		console.log(err);
-	} else {
-		fs.writeFile(filePath, body, 'utf-8');
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8');
+  }
 });
